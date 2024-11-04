87bcf38b7cbd61dbc9f8063840d678d5d6e6e5a8
guille garcia <ggarciacardiel@gmail.com>
2024-11-04 19:24:47 +0100

    creates Navbar and updates layout

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import NavSearch from "./NavSearch";
+import Logo from "./Logo";
+import LinksDropdown from "./LinksDropdown";
+import DarkMode from "./DarkMode";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="border-b border-gray-200">
+      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <LinksDropdown />
+          <DarkMode />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
